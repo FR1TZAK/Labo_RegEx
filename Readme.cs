fc457bf92a163661c12a3b6afaee6fbc236113b5
Sung Lostrie <sung.lostrie@gmail.com>
2021-01-07 18:59:06 +0100

    instructies toegevoegd

---
+++ Readme.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Readme
+    {
+        public Readme()
+        {
+            Debug.WriteLine("Zie Labo_RegEx_Sung.zip in repo voor solution met console app uitgewerkt");
+        }
+    }
+}
